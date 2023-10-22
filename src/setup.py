39603c0f09b6dfbc8be5216fb00d2f87091663a1
Piero <pveravalverde00@mylangara.ca>
2023-10-21 22:32:23 -0700

    initial commit

---
+++ src/setup.py
@@ -0,0 +1,4 @@
+"""Setup script."""
+from setuptools import setup, find_packages
+
+setup(name="titanic", version="0.1", packages=find_packages())
---
+++ src/titanic/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+from pathlib import Path  # pathlib is seriously awesome!
+
+data_dir = Path('../data')
+data_path = data_dir / 'titanic.csv'
